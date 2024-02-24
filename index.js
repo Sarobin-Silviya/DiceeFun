d319ffc4d3e2c528b22ef0217cb2d31585b90576
Sarobin-Silviya <146115745+Sarobin-Silviya@users.noreply.github.com>
2024-02-24 16:58:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage="dice" + randomNumber1 +".png";
+var randomDiceImageSource="images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomDiceImageSource);
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "dice" + randomNumber2 +".png";
+var randomDiceImageSource2 ="images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomDiceImageSource2);
